2c8b78967eac37c5e5635c685191af0a09a284e6
Francisco Javier Rodriguez Hernandez <FJ_RH@hotmail.com>
2021-12-20 19:45:48 -0400

    interface vanillaJS

--- src/server/routes/routes.get.js
+++ src/server/routes/routes.get.js
@@ -4,18 +4,6 @@ const Router = express.Router();
 const controller = require("../controllers/controllers.js")
 
 
-// Router.get("/", async(req, res) => {
-
-//     let materias = {
-//         A1: ["matematica", "ingles"],
-//         B3: ["informatica", "ingles"]
-//     }
-
-//     materias = JSON.stringify(materias);
-//     let response = await controller.insertUser("Francisco Javier", "Rodriguez Hernandez", "Xavier", "123", "16193765", "M", materias, true);
-//     res.json(response);
-// });
-
 Router.get("/", async(req, res) => {
     res.sendFile(path.join(__dirname, "../../client/html/app.html"));
 });
