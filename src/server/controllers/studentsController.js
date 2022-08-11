0aa65d9b1deb28e360e86f8ab1d0075367556f38
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2022-08-11 17:58:33 -0400

    solucionado problema de filro en editar notas

--- src/server/controllers/studentsController.js
+++ src/server/controllers/studentsController.js
@@ -19,9 +19,9 @@ async function registerStudent({ names, lastName, ci, gender, seccion, year, age
         let subjects = {}
 
         filteredSubject.map(e => {
-                subjects[e] = { "l1": 0, "l2": 0, "l3": 0, "def": 0 }
-            })
-            //console.log(subjects)
+            subjects[e] = { "l1": 0, "l2": 0, "l3": 0, "def": 0 }
+        })
+        //console.log(subjects)
 
         let ask = await Students.create({
             names,
@@ -86,24 +86,24 @@ async function findStudent(filters) {
 
 async function updateGrades(list) {
 
-    let student = await findStudent({id:list[0].id})
+    let student = await findStudent({ id: list[0].id })
     let config = await configController.getConfig();
-    
+
 
     if (!config[0].l1 && list[0].subjects[Object.keys(list[0].subjects)[0]].l1) { //primer lapso
-        if(student[0].subjects[Object.keys(list[0].subjects)[0]].l1 != list[0].subjects[Object.keys(list[0].subjects)[0]].l1){
+        if (student[0].subjects[Object.keys(list[0].subjects)[0]].l1 != list[0].subjects[Object.keys(list[0].subjects)[0]].l1) {
             return "No está habilitado el ingreso de notas del primer lapso";
         }
     }
 
     if (!config[0].l2 && list[0].subjects[Object.keys(list[0].subjects)[0]].l2) { //segundo lapso
-        if(student[0].subjects[Object.keys(list[0].subjects)[0]].l2 != list[0].subjects[Object.keys(list[0].subjects)[0]].l2){
+        if (student[0].subjects[Object.keys(list[0].subjects)[0]].l2 != list[0].subjects[Object.keys(list[0].subjects)[0]].l2) {
             return "No está habilitado el ingreso de notas del segundo lapso";
         }
     }
     if (!config[0].l3 && list[0].subjects[Object.keys(list[0].subjects)[0]].l3) { //tercer lapso
 
-        if(student[0].subjects[Object.keys(list[0].subjects)[0]].l3 != list[0].subjects[Object.keys(list[0].subjects)[0]].l3){
+        if (student[0].subjects[Object.keys(list[0].subjects)[0]].l3 != list[0].subjects[Object.keys(list[0].subjects)[0]].l3) {
             return "No está habilitado el ingreso de notas del tercer lapso";
         }
     }
@@ -121,22 +121,43 @@ async function updateGrades(list) {
         let oldSubjects = student[0].subjects;
 
 
+
         if (oldSubjects == null) {
             oldSubjects = list[i].subjects;
 
         } else {
 
+            let keys = Object.keys(list[i].subjects);
+
             if (!config[0].edit) {
-                return "La edición de notas no está permitida en este momento";
-            } else {
-                let keys = Object.keys(list[i].subjects);
+                let message = "La edición de notas no está permitida en este momento";
                 for (let j = 0; j < keys.length; j++) {
-                    oldSubjects[keys[j]] = list[i].subjects[keys[j]];
+                    
+                    if (oldSubjects[keys[j]].l1 != list[i].subjects[keys[j]].l1  && oldSubjects[keys[j]].l1 != 0 ) {
+                        
+                            return message;
+                        
+                    }
+                    if ((oldSubjects[keys[j]].l2 != list[i].subjects[keys[j]].l2) && oldSubjects[keys[j]].l2 != 0 ) {
+                        
+                            return message;
+                        
+                    }
+                    if (oldSubjects[keys[j]].l3 != list[i].subjects[keys[j]].l3  && oldSubjects[keys[j]].l3 != 0 ) {
+                            return message;
+                        
+                    }
+
                 }
+
+            } 
+            for (let j = 0; j < keys.length; j++) {
+                oldSubjects[keys[j]] = list[i].subjects[keys[j]];
             }
+            
 
         }
-       
+
         let update = await Students.update({ subjects: oldSubjects }, {
             where: {
                 id
