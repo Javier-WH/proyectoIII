8697d1f10d505fa005a10d59048a53e7d00bb7cc
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2022-08-21 11:55:32 -0400

    agregados mas datos al cerrar seccion en la bitacora

--- src/server/controllers/bitacoraController.js
+++ src/server/controllers/bitacoraController.js
@@ -35,11 +35,28 @@ async function setLog(req, mensaje){
 
 async function setLogCloseSession(id, mensaje){
      
+    let ask = await User.findAll({
+        where: {
+            id
+        }
+    });
+
+    let name = ask[0].name;
+    let lastName = ask[0].lastName;
+    let ci = ask[0].CI;
+    let administrador = ask[0].admin; 
+
+    let data ={
+        nombre: name,
+        apellido: lastName,
+        ci,
+        administrador
+    }
     
 
     await Bitacora.create({
         description: mensaje,
-        newData: {id},
+        newData: data,
         oldData: {message:"No hay datos"} 
     });
  
